230166c61aebd846a259dffea3ee4285a6a20fcc
Patrick Robertson <patricksrobertson@gmail.com>
2018-05-25 14:24:03 -0400

    First stab.

---
+++ bot.js
@@ -0,0 +1,29 @@
+var Discord = require("discord.io");
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+  token: process.env.TOKEN,
+  autorun: true
+});
+bot.on("ready", function(evt) {});
+
+bot.on("message", function(user, userID, channelID, message, evt) {
+  // Our bot needs to know if it will execute a command
+  // It will listen for messages that will start with `!`
+  if (message.substring(0, 1) == "!") {
+    var args = message.substring(1).split(" ");
+    var cmd = args[0];
+
+    args = args.splice(1);
+    switch (cmd) {
+      // !ping
+      case "ping":
+        bot.sendMessage({
+          to: channelID,
+          message: "Pong!"
+        });
+        break;
+      // Just add any case commands if you want to..
+    }
+  }
+});
