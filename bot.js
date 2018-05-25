acd6813e21ed490fb8d9f14bc74068355deed06d
Patrick Robertson <patricksrobertson@gmail.com>
2018-05-25 14:42:24 -0400

    Add elroy command.

--- bot.js
+++ bot.js
@@ -19,17 +19,17 @@ bot.on("ready", function(evt) {
 bot.on("message", function(user, userID, channelID, message, evt) {
   // Our bot needs to know if it will execute a command
   // It will listen for messages that will start with `!`
-  if (message.substring(0, 1) == "!") {
+  if (message.substring(0, 5) === "!elroy") {
     var args = message.substring(1).split(" ");
     var cmd = args[0];
 
     args = args.splice(1);
     switch (cmd) {
       // !ping
-      case "ping":
+      case "dog":
         bot.sendMessage({
           to: channelID,
-          message: "Pong!"
+          message: "Yes, this is dog"
         });
         break;
       // Just add any case commands if you want to..
