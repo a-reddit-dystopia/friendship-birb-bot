2750ec00819b23d833d8ff764369b1ce4857e6c8
Patrick Robertson <patricksrobertson@gmail.com>
2018-05-25 14:40:39 -0400

    Get this running.

--- bot.js
+++ bot.js
@@ -1,12 +1,21 @@
 var Discord = require("discord.io");
-
+var logger = require("winston");
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+  colorize: true
+});
+logger.level = "debug";
 // Initialize Discord Bot
 var bot = new Discord.Client({
   token: process.env.TOKEN,
   autorun: true
 });
-bot.on("ready", function(evt) {});
-
+bot.on("ready", function(evt) {
+  logger.info("Connected");
+  logger.info("Logged in as: ");
+  logger.info(bot.username + " - (" + bot.id + ")");
+});
 bot.on("message", function(user, userID, channelID, message, evt) {
   // Our bot needs to know if it will execute a command
   // It will listen for messages that will start with `!`
