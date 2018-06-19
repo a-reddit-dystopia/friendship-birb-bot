28573f2be2589537db50da8ce54d9f31f037da1d
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-19 12:44:00 -0400

    Lets go.

--- commands/lottery.js
+++ commands/lottery.js
@@ -28,13 +28,20 @@ function drawWinner(message) {
       logger.debug(response.statusCode);
       const json = JSON.parse(body);
       logger.debug(json);
-      const user = json.data.attributes;
-      logger.debug(user);
-      message.channel.send(
-        `Congrats <@${user.discord_id}> (${user.wow_name} of ${
-          user.wow_server
-        }) is the winner! Follow directions to get your birb!`
-      );
+
+      if (response.statusCode === 201) {
+        const user = json.data.attributes;
+        logger.debug(user);
+        message.channel.send(
+          `Congrats <@${user.discord_id}> (${user.wow_name} of ${
+            user.wow_server
+          }) is the winner! Follow directions to get your birb!`
+        );
+      } else if (response.statusCode === 422) {
+        message.channel.send(`We have drawn all the birbs! Congrats fam!`);
+      } else {
+        message.channel.send(`Arf! Something went horribly wrong.`);
+      }
     }
   );
 }
