165547df47eb0c164bd9f179a995d4387b01f53b
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-19 15:53:26 -0400

    Adllow multiple drawings.

--- commands/lottery.js
+++ commands/lottery.js
@@ -11,13 +11,8 @@ module.exports = {
   name: "lottery",
   description: "Make a birb drawing",
   async execute(client, message, args) {
-    logger.debug(message.guild.channels);
-    logger.debug(message.guild.members);
     const number = args[0] || 1;
     drawWinner(message, number);
-
-    const biebs = await message.guild.member("234134412135104522");
-    biebs.setVoiceChannel("406442986709450755");
   }
 };
 
@@ -38,12 +33,18 @@ function drawWinner(message, number) {
       logger.debug(json);
 
       if (response.statusCode === 201) {
-        const user = json.data.attributes;
-        logger.debug(user);
+        const users = json.data;
+        let msg = [];
+        users.forEach(user => {
+          msg.push(
+            `<@${user.attributes.discord_id}> (${user.attributes.wow_name} of ${
+              user.attributes.wow_server
+            })`
+          );
+        });
+        const text = msg.join(", ");
         message.channel.send(
-          `Congrats <@${user.discord_id}> (${user.wow_name} of ${
-            user.wow_server
-          }) is the winner! Follow directions to get your birb!`
+          `Congrats ${message} are the winner(s)! Follow directions to get your birb!`
         );
       } else if (response.statusCode === 422) {
         message.channel.send(`We have drawn all the birbs! Congrats fam!`);
