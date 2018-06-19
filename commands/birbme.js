415db49508c89dc7355883385ef6c33b37daf272
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-18 21:53:06 -0400

    Fix.

--- commands/birbme.js
+++ commands/birbme.js
@@ -4,9 +4,13 @@ module.exports = {
   execute(message, args) {
     message.react("🤔");
 
-    if (args.length === 3) {
+    if (args.length === 2) {
+      message.channel.send(`${args}`, { reply: message });
     } else {
-      message.channel.send(`${args.length} \n ${args}`, { reply: message });
+      message.channel.send(
+        `I need you to send me your character name, then server name in dash-case, ie bleeding-hollow not Bleeding Hollow`,
+        { reply: message }
+      );
     }
   }
 };
