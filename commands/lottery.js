7338b3f79f603381f121b5f6831410f03207684c
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-19 16:10:59 -0400

    Add admin rights.

--- commands/lottery.js
+++ commands/lottery.js
@@ -11,6 +11,9 @@ module.exports = {
   name: "lottery",
   description: "Make a birb drawing",
   async execute(client, message, args) {
+    if (message.member.roles.find("name", "Elroy Admin")) {
+      return message.reply("Sorry you cannot execute this command.");
+    }
     const number = args[0] || 1;
     drawWinner(message, number);
   }
