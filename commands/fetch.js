42af3e1c22bb28d550b3cbd9147a62b647923b6e
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-19 10:21:26 -0400

    bee smart.

--- commands/fetch.js
+++ commands/fetch.js
@@ -20,11 +20,11 @@ module.exports = {
       },
       (error, response, body) => {
         const json = JSON.parse(body);
-        const users = body.data;
+        const users = body["data"];
         logger.debug(users);
         users.each(user => {
           message.channel.send(
-            `${user.discord_name} ${user.wow_name} ${user.wow_server}`,
+            `${user["discord_name"]} ${user["wow_name"]} ${user["wow_server"]}`,
             { reply: message }
           );
         });
