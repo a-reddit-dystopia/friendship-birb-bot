b453805c77b75c9552552df5957535614d855087
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-19 11:54:48 -0400

    Add some info.

--- commands/lottery.js
+++ commands/lottery.js
@@ -27,12 +27,12 @@ function drawWinner(message) {
     (error, response, body) => {
       const json = JSON.parse(body);
       logger.debug(json);
-      const user = json.data;
+      const user = json.data.attributes;
       logger.debug(user);
       message.channel.send(
-        `Congrats <@${
-          user.attributes.discord_id
-        }> is the winner! Follow directions to get your birb!`
+        `Congrats <@${user.discord_id}> (${user.wow_name} of ${
+          user.wow_server
+        }) is the winner! Follow directions to get your birb!`
       );
     }
   );
