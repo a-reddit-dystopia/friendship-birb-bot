f39ca5ca240145c0c7a5cca184a6131acd4c8e60
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-19 10:19:44 -0400

    Lets get fun.

--- commands/fetch.js
+++ commands/fetch.js
@@ -20,8 +20,14 @@ module.exports = {
       },
       (error, response, body) => {
         const json = JSON.parse(body);
-        logger.debug(body);
-        message.channel.send(json, { reply: message });
+        const users = body.data;
+        logger.debug(users);
+        users.each(user => {
+          message.channel.send(
+            `${user.discord_name} ${user.wow_name} ${user.wow_server}`,
+            { reply: message }
+          );
+        });
       }
     );
   }
