105848a373a5e2419e435fbe696e5c619cbfa24b
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-21 22:49:27 -0400

    It is late.

--- bot.js
+++ bot.js
@@ -34,8 +34,10 @@ client.on("message", async message => {
   );
   if (
     !prefixRegex.test(message.content) ||
-    message.channel.id !== process.env.channel ||
-    message.channel.id !== process.env.TESTCHAN
+    !(
+      message.channel.id === process.env.channel ||
+      message.channel.id === process.env.TESTCHAN
+    )
   )
     return;
 
