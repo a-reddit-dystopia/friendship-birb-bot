72920c6c8a8bf19e7e154d15970293c7e6265154
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-21 22:30:34 -0400

    Add basic help command.

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "ping",
+  description: "Ping!",
+  execute(client, message, args) {
+    if (message.member.roles.find("name", "Elroy Admin")) {
+      // admin help
+    }
+    const msg = `Elroy help command!
+** Fetch ** - retrieve the contestant link
+elroy fetch
+** Birbme ** - get yourself on the friendship birb list
+elroy birbme *character name* *server name* (be sure to use dashes instead of spaces on server name!)`;
+    message.reply(msg);
+  }
+};
