a0e79e90bd1e9fcd629d5c68c361a873d25c192e
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-21 22:36:54 -0400

    admin message.

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,16 @@ module.exports = {
   description: "help command",
   execute(client, message, args) {
     if (message.member.roles.find("name", "Elroy Admin")) {
-      // admin help
+      const adminMsg = `Elroy Admin help command!
+** Add ** - adds the 'AOTC Winner' role to discord user(s)
+elroy add @biebear (you can tag multiple people)
+** Yank ** - removes the 'AOTC Winner role from discord user(s)
+elroy yank @pip (you can tag multiple people)
+** Remove ** - takes a discord user off the contestant list
+elroy remove @sharrq (you can tag multiple people)
+** Lottery ** - announces N users as winners
+elroy lottery 3 (announces 3 winners)`;
+      message.author.send(adminMsg);
     }
     const msg = `Elroy help command!
 ** Fetch ** - retrieve the contestant link
