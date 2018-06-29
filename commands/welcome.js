0189250f7336c5108eb8944d337eadc63d8b4789
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-29 17:13:08 -0400

    ladyraid

--- commands/welcome.js
+++ commands/welcome.js
@@ -72,6 +72,9 @@ module.exports = {
     const pushing = await message.guild.roles
       .find(role => role.name === "M+Pushing")
       .toString();
+    const ladyRaid = await message.guild.roles
+      .find(role => role.name === "Ladyraid")
+      .toString();
     const WELCOME_BODY = `We are a cross-realm US-Horde community that was founded from a single Reddit post early in the Legion expansion. We pride ourselves on being an open and inclusive laid back group of gamers. We have an official guild on Horde-Bleeding Hollow. You DO NOT have to transfer or join our guild, but if you are interested in doing so, just speak to one of the ${officers}/${mvps}s and they will point you in the right direction.
     `;
     //prettier-ignore
@@ -103,7 +106,7 @@ ${nsfwChat}: This channel should be fairly self-explanatory. It is opt-in only a
     const OPT_IN_BODY = `**World of Warcraft**
 
 - ${mythicPlusChat} (and: ${tank}, ${healer}, ${mdps}, ${rdps},  ${casual}, ${pushing})
-- Lady Raid
+- ${ladyRaid}
 - Bleeding Hollow
 - Community Member
 - PVP
