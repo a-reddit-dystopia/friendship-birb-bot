df6d2f7f721cf253bcf1fd42405968d088a0ac02
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-29 16:19:48 -0400

    abort

--- commands/welcome.js
+++ commands/welcome.js
@@ -9,18 +9,6 @@ module.exports = {
   name: "welcome",
   description: "Welcome ARD Message",
   async execute(client, message, args) {
-    const reactionNumbers = [
-      "\u0030\u20E3",
-      "\u0031\u20E3",
-      "\u0032\u20E3",
-      "\u0033\u20E3",
-      "\u0034\u20E3",
-      "\u0035\u20E3",
-      "\u0036\u20E3",
-      "\u0037\u20E3",
-      "\u0038\u20E3",
-      "\u0039\u20E3"
-    ];
     const generalChannel = message.guild.channels
       .find(channel => channel.name === "general")
       .toString();
@@ -30,23 +18,21 @@ module.exports = {
     const mvps = message.guild.roles
       .find(role => role.name === "MVP")
       .toString();
-
     const WELCOME_BODY = `We are a cross-realm US-Horde community that was founded from a single Reddit post early in the Legion expansion. We pride ourselves on being an open and inclusive laid back group of gamers. We have an official guild on Horde-Bleeding Hollow. You DO NOT have to transfer or join our guild, but if you are interested in doing so, just speak to one of the ${officers}/${mvps}s and they will point you in the right direction.
     `;
-    //prettier-ignore
-    const RULES_BODY = `${reactionNumbers[1]} **We do not tolerate abusive behavior of any type.**
+    const RULES_BODY = `1) **We do not tolerate abusive behavior of any type.**
 Many of our members are casual players, and everyone was new at one point. If you arenít here to have a good time, help people learn, and make a few jokes along the way, it may be best if you move along.
 
-${reactionNumbers[2]} **No politics.  No religion.**
+2) **No politics.  No religion.**
 Just not worth the headache.
 
-${reactionNumbers[3]} **No inappropriate names or profile pictures**.
+3) **No inappropriate names or profile pictures**.
 Our ${generalChannel} channel is PG-13. We do not allow for lewd or inflammatory user names or discord profile pictures.
 
-${reactionNumbers[4]} **Zero tolerance for sales**
+4) **Zero tolerance for sales**
 We do not allow for the sale, for either in-game currency, or real world money, of any runs in this discord.  Please do not advertise any raids or groups that partake in these practices. Advertising sales in thsi discord will result in an immediate and permanent ban.
 
-${reactionNumbers[5]} **Rules and Guidelines Documents** (these will eventually link to something)
+5) **Rules and Guidelines Documents** (these will eventually link to something)
 Community Raid Guidelines
 Progression Raid Guidelines
 Outside Guild Recruitment Rules`;
