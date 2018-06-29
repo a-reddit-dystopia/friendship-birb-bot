03057bd857dc49ff71996c63a4eb2fadb36b7360
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-29 16:16:23 -0400

    fix?

--- commands/welcome.js
+++ commands/welcome.js
@@ -30,11 +30,11 @@ module.exports = {
     const mvps = message.guild.roles
       .find(role => role.name === "MVP")
       .toString();
+
     const WELCOME_BODY = `We are a cross-realm US-Horde community that was founded from a single Reddit post early in the Legion expansion. We pride ourselves on being an open and inclusive laid back group of gamers. We have an official guild on Horde-Bleeding Hollow. You DO NOT have to transfer or join our guild, but if you are interested in doing so, just speak to one of the ${officers}/${mvps}s and they will point you in the right direction.
     `;
-    const RULES_BODY = `${
-      reactionNumbers[1]
-    } **We do not tolerate abusive behavior of any type.**
+    //prettier-ignore
+    const RULES_BODY = `${reactionNumbers[1]} **We do not tolerate abusive behavior of any type.**
 Many of our members are casual players, and everyone was new at one point. If you arenít here to have a good time, help people learn, and make a few jokes along the way, it may be best if you move along.
 
 ${reactionNumbers[2]} **No politics.  No religion.**
@@ -46,9 +46,7 @@ Our ${generalChannel} channel is PG-13. We do not allow for lewd or inflammatory
 ${reactionNumbers[4]} **Zero tolerance for sales**
 We do not allow for the sale, for either in-game currency, or real world money, of any runs in this discord.  Please do not advertise any raids or groups that partake in these practices. Advertising sales in thsi discord will result in an immediate and permanent ban.
 
-${
-      reactionNumbers[5]
-    } **Rules and Guidelines Documents** (these will eventually link to something)
+${reactionNumbers[5]} **Rules and Guidelines Documents** (these will eventually link to something)
 Community Raid Guidelines
 Progression Raid Guidelines
 Outside Guild Recruitment Rules`;
