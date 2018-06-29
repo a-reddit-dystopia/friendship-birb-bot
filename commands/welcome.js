633e9a3f401078d908c56ab7c8430761678513aa
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-29 17:19:35 -0400

    cm

--- commands/welcome.js
+++ commands/welcome.js
@@ -78,6 +78,9 @@ module.exports = {
     const bh = await message.guild.roles
       .find(role => role.name === "Bleeding Hollow")
       .toString();
+    const cm = await message.guild.roles
+      .find(role => role.name === "Community Member")
+      .toString();
     const WELCOME_BODY = `We are a cross-realm US-Horde community that was founded from a single Reddit post early in the Legion expansion. We pride ourselves on being an open and inclusive laid back group of gamers. We have an official guild on Horde-Bleeding Hollow. You DO NOT have to transfer or join our guild, but if you are interested in doing so, just speak to one of the ${officers}/${mvps}s and they will point you in the right direction.
     `;
     //prettier-ignore
@@ -111,7 +114,7 @@ ${nsfwChat}: This channel should be fairly self-explanatory. It is opt-in only a
 - ${mythicPlusChat} (and: ${tank}, ${healer}, ${mdps}, ${rdps},  ${casual}, ${pushing})
 - ${ladyRaid}
 - ${bh}
-- Community Member
+- ${cm}
 - PVP
 - Beta
 
