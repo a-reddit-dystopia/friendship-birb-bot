207b214739c0346f988ddade21d5e32b558e3062
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-29 15:53:03 -0400

    lets go

--- commands/welcome.js
+++ commands/welcome.js
@@ -3,19 +3,7 @@ const WELCOME_BODY = `We are a cross-realm US-Horde community that was founded f
 `;
 
 const RULES_TITLE = "Community Rules";
-const RULES_BODY = `1) **We do not tolerate abusive behavior of any type.**
-Many of our members are casual players, and everyone was new at one point. If you arenít here to have a good time, help people learn, and make a few jokes along the way, it may be best if you move along.
-2) **No politics.  No religion.**
-Just not worth the headache.
-3) **No inappropriate names or profile pictures**.
-Our <#${generalChannel}> channel is PG-13. We do not allow for lewd or inflammatory user names or discord profile pictures.
-4) **Zero tolerance for sales**
-We do not allow for the sale, for either in-game currency, or real world money, of any runs in this discord.  Please do not advertise any raids or groups that partake in these practices. Advertising sales in thsi discord will result in an immediate and permanent ban.
-5) **Rules and Guidelines Documents** (these will eventually link to something)
-Community Raid Guidelines
-Progression Raid Guidelines
-Outside Guild Recruitment Rules
-------------------------------------------------------------------
+const RULES_FOOTER = `------------------------------------------------------------------
 The rules are not inclusive and detailed so they are subject to officer discretion. Not being able to follow these rules will result in consequences decided by officers.`;
 
 module.exports = {
@@ -25,6 +13,22 @@ module.exports = {
     const generalChannel = message.guild.channels
       .find(channel => channel.name === "general")
       .toString();
+    const RULES_BODY = `1) **We do not tolerate abusive behavior of any type.**
+Many of our members are casual players, and everyone was new at one point. If you arenít here to have a good time, help people learn, and make a few jokes along the way, it may be best if you move along.
+
+2) **No politics.  No religion.**
+Just not worth the headache.
+
+3) **No inappropriate names or profile pictures**.
+Our <#${generalChannel}> channel is PG-13. We do not allow for lewd or inflammatory user names or discord profile pictures.
+
+4) **Zero tolerance for sales**
+We do not allow for the sale, for either in-game currency, or real world money, of any runs in this discord.  Please do not advertise any raids or groups that partake in these practices. Advertising sales in thsi discord will result in an immediate and permanent ban.
+
+5) **Rules and Guidelines Documents** (these will eventually link to something)
+Community Raid Guidelines
+Progression Raid Guidelines
+Outside Guild Recruitment Rules`;
     const welcome = {
       color: 3447003,
       title: WELCOME_TITLE,
@@ -35,7 +39,10 @@ module.exports = {
     const rules = {
       color: 3447003,
       title: RULES_TITLE,
-      description: RULES_BODY
+      description: RULES_BODY,
+      footer: {
+        text: RULES_FOOTER
+      }
     };
     await message.channel.send({ embed: rules });
   }
