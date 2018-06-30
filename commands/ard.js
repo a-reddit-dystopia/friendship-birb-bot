eb9627b971c857d669725ea3e068c49401587609
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-30 06:48:50 -0400

    whooops

--- commands/ard.js
+++ commands/ard.js
@@ -13,9 +13,9 @@ module.exports = {
       .toString();
 
     const WELCOME_BODY = `We are a cross-realm US-Horde community that was founded from a single Reddit post early in the Legion expansion. We pride ourselves on being an open and inclusive laid back group of gamers. We have an official guild on Horde-Bleeding Hollow. You DO NOT have to transfer or join our guild, but if you are interested in doing so, just speak to one of the ${officers}/${mvps}s and they will point you in the right direction.
-    `;
+`;
 
-    const emded = {
+    const embed = {
       color: 3447003,
       title: WELCOME_TITLE,
       description: WELCOME_BODY
