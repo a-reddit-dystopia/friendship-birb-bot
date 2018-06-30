5c847da6f3c6e69d463db3970e7884a0147b7cf3
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-30 06:54:23 -0400

    rules

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,29 +1,15 @@
-const RULES_TITLE = "Community Rules";
-const RULES_FOOTER = `The rules are not inclusive and detailed so they are subject to officer discretion. Not being able to follow these rules will result in consequences decided by officers.`;
 const LINKS_TITLE = `Helpful Links`;
 const LINKS_BODY = `[Our subreddit](https://www.reddit.com/r/WoWARD/)
 [ARD FAQ](https://www.buzzfeed.com/mjs538/the-25-cutest-corgi-puppies-currently-online?utm_term=.fpX77bMnOA#.hnkdd6xZrM)`;
 const CHANNELS_TITLE = "Channels";
 const OPT_IN_TITLE = "Opt-in Roles and Channels";
 const ard = require("./ard.js");
+const rules = require("./rules.js");
 
 module.exports = {
   name: "welcome",
   description: "Welcome ARD Message",
   async execute(client, message, args) {
-    const reactionNumbers = [
-      "\u0030\u20E3",
-      "\u0031\u20E3",
-      "\u0032\u20E3",
-      "\u0033\u20E3",
-      "\u0034\u20E3",
-      "\u0035\u20E3",
-      "\u0036\u20E3",
-      "\u0037\u20E3",
-      "\u0038\u20E3",
-      "\u0039\u20E3"
-    ];
-
     // chans
     const generalChannel = await message.guild.channels
       .find(channel => channel.name === "general")
@@ -76,24 +62,6 @@ module.exports = {
       .find(role => role.name.toLowerCase() === "beta")
       .toString();
 
-    //prettier-ignore
-    const RULES_BODY = `${reactionNumbers[1]} **We do not tolerate abusive behavior of any type.**
-Many of our members are casual players, and everyone was new at one point. If you arenít here to have a good time, help people learn, and make a few jokes along the way, it may be best if you move along.
-
-${reactionNumbers[2]} **No politics.  No religion.**
-Just not worth the headache.
-
-${reactionNumbers[3]} **No inappropriate names or profile pictures**.
-Our ${generalChannel} channel is PG-13. We do not allow for lewd or inflammatory user names or discord profile pictures.
-
-${reactionNumbers[4]} **Zero tolerance for sales**
-We do not allow for the sale, for either in-game currency, or real world money, of any runs in this discord.  Please do not advertise any raids or groups that partake in these practices. Advertising sales in thsi discord will result in an immediate and permanent ban.
-
-${reactionNumbers[5]} **Rules and Guidelines Documents** (these will eventually link to something)
-Community Raid Guidelines
-Progression Raid Guidelines
-Outside Guild Recruitment Rules`;
-
     const CHANNELS_BODY = `${generalChannel}: Will be kept roughly PG in order to maintain a more welcoming public facing channel for people that might be checking out the guild for the first time or who may not have decided to take the plunge into ARD yet. It will be a good place for people new to ARD to ask questions and get to know people here. This channel is also where you will find advertising for any ARD sponsored content. These groups are run under ARD rules, within the ARD Discord.
 
 ${ardChat}: It is now visible to everyone green and up, meaning everyone that has chosen to become a part of the ARD community and are thus willing to accept all that comes with it. ARD rules will still apply in there, so keep it classy and don't be a douche canoe. Moderators will still be keeping an eye on things in there, but the reins will be a bit looser than general.
@@ -124,15 +92,7 @@ ${nsfwChat}: This channel should be fairly self-explanatory. It is opt-in only a
 - NSFW
 - Spoiled`;
     await ard.execute(client, message, args);
-    const rules = {
-      color: 3447003,
-      title: RULES_TITLE,
-      description: RULES_BODY,
-      footer: {
-        text: RULES_FOOTER
-      }
-    };
-    await message.channel.send({ embed: rules });
+    await rules.execute(client, message, args);
 
     const links = {
       color: 3447003,
