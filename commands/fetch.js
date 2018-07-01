e3fa15190be265a585a0db8e677d1f393eb4847a
Patrick Robertson <patricksrobertson@gmail.com>
2018-07-01 06:45:43 -0400

    get things.

--- commands/fetch.js
+++ commands/fetch.js
@@ -1,7 +1,17 @@
+const request = require("request-promise-native");
+const logger = require("winston");
+
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+  colorize: true
+});
+logger.level = "debug";
+
 module.exports = {
   name: "fetch",
   description: "Stop trying to make fetch happen",
-  execute(client, message, args) {
+  async execute(client, message, args) {
+    await getCount();
     const embed = {
       color: 3447003,
       author: {
@@ -20,3 +30,15 @@ module.exports = {
     message.channel.send({ embed });
   }
 };
+
+async function getCount() {
+  const response = await request.get(`${process.env.API}api/users.json`, {
+    auth: {
+      bearer: process.env.elroy
+    },
+    form: {}
+  });
+
+  const json = JSON.parse(response.body);
+  logger.debug(json);
+}
