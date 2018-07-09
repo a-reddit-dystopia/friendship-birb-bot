470e62fb2c786e34618b1733442d4c322ccdfb92
Patrick Robertson <patricksrobertson@gmail.com>
2018-07-09 11:16:36 -0400

    Back to formatting.

--- commands/ard.js
+++ commands/ard.js
@@ -15,7 +15,7 @@ module.exports = {
       .find(role => role.name === "Moderator")
       .toString();
 
-    const WELCOME_BODY = `Welcome to A Reddit Dystopia! We are a cross-realm US Horde community that prides ourselves on being open and inclusive. We have an official guild on Bleeding Hollow. There is no requirement to join the actual guild to participate in any event that can be done cross realm. If you have questions that are not covered below, please speak to one of the ${officers}/${mods}/${mvps}s and they will point you in the right direction.`;
+    const WELCOME_BODY = `Welcome to A Reddit Dystopia! We are a cross-realm US-Horde community that prides ourselves on being open and inclusive. We have an official guild on Bleeding Hollow that anyone can join at any time. There is no requirement to join the guild or server transfer to participate in any event that can be done cross-realm. If you have questions that are not covered below, please speak to one of the ${officers}/${mods}/${mvps}s and they will point you in the right direction.`;
     const embed = {
       color: 3447003,
       title: WELCOME_TITLE,
