26dfdfa17d5d2adb4d8c207836ba9ea8ed593a6e
Patrick Robertson <patricksrobertson@gmail.com>
2018-07-14 10:46:03 -0400

    I am a genius

--- commands/add.js
+++ commands/add.js
@@ -15,7 +15,7 @@ module.exports = {
     const taggedUsers = message.mentions.users;
     logger.debug(taggedUsers);
 
-    if (taggedUsers <= 10) {
+    if (taggedUsers.length <= 10) {
       const role = message.guild.roles.find("name", "AOTC Winner");
 
       taggedUsers.forEach(async function(user) {
