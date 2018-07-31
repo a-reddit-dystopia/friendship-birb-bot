34722d0bc705e7c09eed879de401e962a249f327
Patrick Robertson <patricksrobertson@gmail.com>
2018-07-31 09:15:08 -0400

    Lets try this.

--- commands/set.js
+++ commands/set.js
@@ -2,7 +2,8 @@ module.exports = {
   name: "set",
   description: "Set config options",
   execute(client, message, args, state) {
-    const [actionType, actionValue] = args;
+    const actionType = args[0];
+    const actionValue = args.shift().join(" ");
 
     if (actionType === "lottery") {
       state.lotteryEnabled = actionValue === "enabled" ? true : false;
