1a7e79bc40efc700c3fbd9f35c33512fe92b15f8
Patrick Robertson <patricksrobertson@gmail.com>
2018-07-31 09:24:32 -0400

    woo.

--- commands/set.js
+++ commands/set.js
@@ -2,8 +2,8 @@ module.exports = {
   name: "set",
   description: "Set config options",
   execute(client, message, args, state) {
-    const actionType = args[0];
-    const actionValue = args.shift().join(" ");
+    const actionType = args.shift();
+    const actionValue = args.join(" ");
 
     if (actionType === "lottery") {
       state.lotteryEnabled = actionValue === "enabled" ? true : false;
