56b409db27f8052762e854ce6206201180e75d74
Patrick Robertson <patricksrobertson@gmail.com>
2020-05-04 18:07:40 -0400

    Debug

--- commands/enter.js
+++ commands/enter.js
@@ -81,8 +81,6 @@ module.exports = {
 async function doTheRequest(charName, serverName, errorBuilder, state) {
   try {
     blizz.defaults.token = state.token;
-    console.log(state.token);
-    console.log(blizz.defaults.token);
 
     const char = await blizz.wow.character("achievements", {
       region: "us",
@@ -98,16 +96,16 @@ async function doTheRequest(charName, serverName, errorBuilder, state) {
     console.log(birbie);
     const hasBirb = allAchievements.includes(process.env.AOTC_ID);
     console.log(hasBirb);
-    if (char.data.faction === 0) {
-      errorBuilder.status = "not_ok";
-      errorBuilder.errors.character.push(NOT_HORDE);
-      return ["not_ok", NOT_HORDE];
-    } else if (hasBirb === true) {
-      errorBuilder.status = "not_ok";
-      errorBuilder.errors.birb.push(HAS_BIRB);
-      return ["not_ok", HAS_BIRB];
-    }
-    return ["ok"];
+    // if (char.data.faction === 0) {
+    //   errorBuilder.status = "not_ok";
+    //   errorBuilder.errors.character.push(NOT_HORDE);
+    //   return ["not_ok", NOT_HORDE];
+    // } else if (hasBirb === true) {
+    //   errorBuilder.status = "not_ok";
+    //   errorBuilder.errors.birb.push(HAS_BIRB);
+    //   return ["not_ok", HAS_BIRB];
+    // }
+    // return ["ok"];
   } catch (error) {
     errorBuilder.status = "not_ok";
     const reason = error.response.data.reason;
